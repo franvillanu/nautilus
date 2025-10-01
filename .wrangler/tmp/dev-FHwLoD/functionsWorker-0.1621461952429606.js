4ceee16dfb2958484b5201ff3872a7c5c5cd5a13
franvillanu <franciscovillanu@gmail.com>
2025-10-01 19:08:26 +0100

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- .wrangler/tmp/dev-FHwLoD/functionsWorker-0.1621461952429606.js
+++ .wrangler/tmp/dev-FHwLoD/functionsWorker-0.1621461952429606.js
@@ -704,7 +704,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-b0suPd/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-MXZt5Z/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -736,7 +736,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-b0suPd/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-MXZt5Z/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
